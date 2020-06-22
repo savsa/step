fd4de9b5a724490c61803fd513e4e39a46d7ebe8
savsa <savsample@gmail.com>
2020-06-22 23:02:59 +0000

    Implement FindMeetingQuery.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,51 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
+/* Given the meeting information, return the times when the meeting could happen that day. */
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+      return Arrays.asList();
+    }
+
+    if (events.isEmpty() || request.getAttendees().isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+
+    Collection<String> requestAttendees = request.getAttendees();
+    ArrayList<TimeRange> eventTimes = new ArrayList<TimeRange>();
+    for (Event event : events) {
+      ArrayList<String> relevantAttendees = new ArrayList<String>(request.getAttendees());
+      relevantAttendees.retainAll(event.getAttendees());
+      if (relevantAttendees.isEmpty()) {
+        continue;
+      }
+      eventTimes.add(event.getWhen());
+    }
+
+    eventTimes.add(TimeRange.fromStartDuration(TimeRange.END_OF_DAY + 1, 0));
+    Collections.sort(eventTimes, TimeRange.ORDER_BY_START);
+    int startTime = TimeRange.START_OF_DAY;
+    long duration = request.getDuration();
+    Collection<TimeRange> availableTimes = new ArrayList<TimeRange>();
+    for (TimeRange eventTime : eventTimes) {
+      if (eventTime.end() < startTime) {
+        continue;
+      }
+      if (eventTime.start() < startTime) {
+        startTime = eventTime.end();
+        continue;
+      }
+      if (eventTime.start() - startTime >= request.getDuration()) {
+        availableTimes.add(TimeRange.fromStartEnd(startTime, eventTime.start(), false));
+      }
+      startTime = eventTime.end();
+    }
+    return availableTimes;
   }
 }
